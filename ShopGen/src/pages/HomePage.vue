794162fec626de25441c75d0aa702bc55a33bd06
Trevor Morris <tremorris1999@gmail.com>
2023-01-13 06:38:02 +0000

    chore(web): adds home page

--- ShopGen/src/pages/HomePage.vue
+++ ShopGen/src/pages/HomePage.vue
@@ -1,7 +1,24 @@
 <template>
-  <HelloWorld />
+  <v-card
+    class="mt-4 px-4 pb-8 text-center"
+    width="336px"
+    title="Welcome to ShopGen"
+  >
+    <lottie-player
+      src="https://assets5.lottiefiles.com/packages/lf20_z9agdrw6.json"
+      background="transparent"
+      speed="0.4"
+      style="width: 300px; height: 300px"
+      loop
+      autoplay
+    />
+    <span>Press the </span>
+    <v-icon class="mx-2" icon="mdi-menu" />
+    <span>
+      icon in the upper left corner to get access the application menu and get
+      started.
+    </span>
+  </v-card>
 </template>
 
-<script setup lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
